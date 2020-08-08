d18198b89c02a47aa5e3a20a61fdb04cf0f4dbe0
nickperov <nikolay.perov@gmail.com>
2020-08-08 14:20:46 +1200

    add POST method with class body and Logging

--- graalvm-springboot-h2-rest/src/main/java/com/nickperov/stud/graalvm/spring/h2_rest/H2DBAppController.java
+++ graalvm-springboot-h2-rest/src/main/java/com/nickperov/stud/graalvm/spring/h2_rest/H2DBAppController.java
@@ -5,18 +5,21 @@ import com.nickperov.stud.graalvm.spring.h2_rest.db.NoteRepository;
 import com.nickperov.stud.graalvm.spring.h2_rest.model.NoteDTO;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class H2DBAppController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(H2DBAppController.class);
 
     @Autowired
     private NoteRepository noteRepository;
@@ -28,22 +31,25 @@ public class H2DBAppController {
 
     @GetMapping("/note")
     public NoteDTO getNote(@RequestParam final long id) {
-        return new NoteDTO(noteRepository.getOne(id));
+        Optional<NoteEntity> queryResult = noteRepository.findById(id);
+        if (queryResult.isPresent()) {
+            LOGGER.info("Note with id: {} found", id);
+            return new NoteDTO(queryResult.get());
+        } else {
+            LOGGER.info("Note with id: {} not found", id);
+            return new NoteDTO();
+        }
     }
 
     @PostMapping("/note")
-    public String addNote(@RequestBody final String note) {
-        return "Reply to note " + note;
+    public String addNote(@RequestBody final NoteDTO note) {
+        final NoteEntity noteEntity = noteRepository.save(new NoteEntity(note));
+        LOGGER.info("New note created: {}", noteEntity);
+        return "" + noteEntity.getId();
     }
-    
-    /*@PostMapping("/note")
-    public void addNote(@RequestBody final NoteDTO note) {
-        noteRepository.save(new NoteEntity(note));
-    }*/
 
     @GetMapping("/notes")
     public List<NoteDTO> listNotes() {
         return noteRepository.findAll().stream().map(NoteDTO::new).collect(Collectors.toList());
     }
-
 }
